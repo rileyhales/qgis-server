5b06aca3ec4e013d44d0af2b95bfbfeca31acd32
rileyhales <rileyhales1@gmail.com>
2024-04-13 12:42:09 -0600

    add qgis server envs

--- docker.sh
+++ docker.sh
@@ -2,9 +2,11 @@ docker network create qgis
 docker run \
   --name qgis-server \
   --net=qgis --hostname=qgis-server \
-  -v /Users/rchales/code/qgis-server/data/:/data:ro \
+  -v ./data/:/data:ro \
   -p 5555:5555 \
   -e "QGIS_PROJECT_FILE=/data/project.qgs" \
+  -e "QGIS_PARALLEL_RENDERING=1" \
+  -r "QGIS_MAX_THREADS=8" \
   --init \
   --rm \
   qgis-server
